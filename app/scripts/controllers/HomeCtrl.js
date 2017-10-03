8cecf98f1d15b2e9f5b721432938345692cc580b
AKW <Kaeleigh@users.noreply.github.com>
2017-10-03 11:22:04 -0400

    messages listed success

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,12 +2,13 @@
     function HomeCtrl(Room, Message) {
       this.rooms = Room.all;         // array rooms is attached to HomeCtrl and array of objects retrieved by all method
 
+
       this.openRoom = function(room) {      //set value to and filters messages of current room
         var currentRoom = room;
         this.currentRoomName = currentRoom.$value;
         this.messages = Message.getByRoomId(currentRoom.$id);
-
       }
+
     }
 
     angular
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,9 +3,14 @@
     var Message = {};
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
+  //  console.log("Message Service");
+  //  console.log(messages);
 
     Message.getByRoomId = function(roomId) {
-      return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));      //filters messages by roomid
+    //  console.log(roomId);
+      return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));      //filters messages by roomid, set return list of messages quered from all attribute
+    //  console.log(results);
+    //  return results;
     };
     return Message;
   }
